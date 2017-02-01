0043d5e7d886afffed7a4b5490374ebc6fe081cb
patrickAbbs <patrick.abbs@gmail.com>
2017-02-01 16:03:05 -0600

    PoC weighting on random pixel selection towards known, high-laplace value pixels

--- Eyebot3/CornerModule.cs
+++ Eyebot3/CornerModule.cs
@@ -15,6 +15,8 @@ namespace Eyebot3
         private Bitmap perceivedImage { get; set; }
         private Bitmap realImage { get; set; }
         private LaplaceFilter laplacer { get; set; }
+        private Dictionary<Tuple<int, int>, int> knownPixels { get; set; }
+        private int imageSize { get; set; }
 
         public CornerModule(Bitmap image)
         {
@@ -24,29 +26,49 @@ namespace Eyebot3
             perceivedImage = new Bitmap(xSize, ySize);
             realImage = image;
             laplacer = new LaplaceFilter();
+            knownPixels = new Dictionary<Tuple<int, int>, int>();
+            imageSize = image.Width * image.Height;
         }
 
-        //public Tuple<int, int> nextLocationStrategy()
+        public Tuple<int, int> nextLocationStrategy()
+        {
+            if(die.NextDouble() * imageSize > knownPixels.Count)
+            {
+                return new Tuple<int, int>((int)(die.NextDouble() * xSize), (int)(die.NextDouble() * ySize));
+            }
+            //arbitrary cutoff choice and massively inefficient with the resorting every round, but this is POC
+            var sortedOrder = knownPixels.Where(x => x.Value > 10).OrderByDescending(x => x.Value).ToList();
+            var entryChoice = (int)(Math.Pow(die.NextDouble(), 2) * sortedOrder.Count);
+            var baseEntry = sortedOrder[entryChoice].Key;
+            var chosenX = (int)(die.NextDouble() * 20 - 10);
+            var chosenY = (int)(die.NextDouble() * 20 - 10);
+            var chosenEntry = new Tuple<int, int>(baseEntry.Item1 + chosenX, baseEntry.Item2 + chosenY);
+            return chosenEntry;
+            //
+
+        }
 
         public void laplaceCaller()
         {
             var directory = System.IO.Directory.GetCurrentDirectory();
-            //for (int i = 0; i < 1000; i++)
-            var knownPixels = new Dictionary<Tuple<int, int>, int>();
             var counter = 0;
             while (true)
             {
-                int xLocation = (int)(die.NextDouble() * xSize);
-                int yLocation = (int)(die.NextDouble() * ySize);
-                //if (xLocation > 5 && yLocation > 5 && xLocation < xSize - 5 && yLocation < ySize - 5)
-                //{
+                //int xLocation = (int)(die.NextDouble() * xSize);
+                //int yLocation = (int)(die.NextDouble() * ySize);
+                var nextLocationTuple = nextLocationStrategy();
+                int xLocation = nextLocationTuple.Item1;
+                int yLocation = nextLocationTuple.Item2;
+
+                if (xLocation >= 0 && yLocation >= 0 && xLocation < xSize && yLocation < ySize)
+                {
                     var pixelBrightness = laplacer.getLaplaceVal(1, 2, .5, xLocation, yLocation, realImage);
-                    perceivedImage.SetPixel(xLocation,
-                        yLocation, Color.FromArgb(255, pixelBrightness, pixelBrightness, pixelBrightness));
-                    //knownPixels[new Tuple<int, int>(xLocation, yLocation)] =  pixelBrightness;
-                //}
+                    perceivedImage.SetPixel(xLocation, yLocation, Color.FromArgb(255, pixelBrightness, pixelBrightness, pixelBrightness));
+                    knownPixels[new Tuple<int, int>(xLocation, yLocation)] = pixelBrightness;
+                }
+
                 counter++;
-                if(counter > 5000)
+                if(counter > 20000)
                 {
                     counter = 0;
                     perceivedImage.Save(directory + "/Images/laplacedTriangle.png");
