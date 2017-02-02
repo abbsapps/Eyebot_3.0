b5154041cb8a774dff97bd598449cc7fac7f88cb
patrickAbbs <patrick.abbs@gmail.com>
2017-02-02 16:28:25 -0600

    trying to replace inefficient semi-random pixel-choice sort code, thusfar progress being a lot of replacement that seems to still be very inefficient

--- Eyebot3/CornerModule.cs
+++ Eyebot3/CornerModule.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Specialized;
 
 namespace Eyebot3
 {
@@ -16,6 +17,8 @@ namespace Eyebot3
         private Bitmap realImage { get; set; }
         private LaplaceFilter laplacer { get; set; }
         private Dictionary<Tuple<int, int>, int> knownPixels { get; set; }
+        private List<int> orderedBrightnesses { get; set; }
+        private List<Tuple<int, int>> orderedPixels { get; set; }
         private int imageSize { get; set; }
 
         public CornerModule(Bitmap image)
@@ -27,6 +30,8 @@ namespace Eyebot3
             realImage = image;
             laplacer = new LaplaceFilter();
             knownPixels = new Dictionary<Tuple<int, int>, int>();
+            orderedBrightnesses = new List<int>() { 0 };
+            orderedPixels = new List<Tuple<int, int>>() { new Tuple<int, int>(0,0)};
             imageSize = image.Width * image.Height;
         }
 
@@ -36,16 +41,12 @@ namespace Eyebot3
             {
                 return new Tuple<int, int>((int)(die.NextDouble() * xSize), (int)(die.NextDouble() * ySize));
             }
-            //arbitrary cutoff choice and massively inefficient with the resorting every round, but this is POC
-            var sortedOrder = knownPixels.Where(x => x.Value > 10).OrderByDescending(x => x.Value).ToList();
-            var entryChoice = (int)(Math.Pow(die.NextDouble(), 2) * sortedOrder.Count);
-            var baseEntry = sortedOrder[entryChoice].Key;
+            var entryChoice = (int)(Math.Pow(die.NextDouble(), 5) * orderedPixels.Count);
+            var baseEntry = orderedPixels[orderedPixels.Count - entryChoice - 1];
             var chosenX = (int)(die.NextDouble() * 20 - 10);
             var chosenY = (int)(die.NextDouble() * 20 - 10);
             var chosenEntry = new Tuple<int, int>(baseEntry.Item1 + chosenX, baseEntry.Item2 + chosenY);
             return chosenEntry;
-            //
-
         }
 
         public void laplaceCaller()
@@ -60,15 +61,32 @@ namespace Eyebot3
                 int xLocation = nextLocationTuple.Item1;
                 int yLocation = nextLocationTuple.Item2;
 
+                var pixelBrightness = laplacer.getLaplaceVal(1, 2, .5, xLocation, yLocation, realImage);
+
                 if (xLocation >= 0 && yLocation >= 0 && xLocation < xSize && yLocation < ySize)
                 {
-                    var pixelBrightness = laplacer.getLaplaceVal(1, 2, .5, xLocation, yLocation, realImage);
+                    var pixelLocation = new Tuple<int, int>(xLocation, yLocation);
+                    if (knownPixels.ContainsKey(pixelLocation))
+                    {
+                        var listIndex = orderedPixels.IndexOf(pixelLocation);
+                        orderedPixels.RemoveAt(listIndex);
+                        orderedBrightnesses.RemoveAt(listIndex);
+                    }
+                    knownPixels[pixelLocation] = pixelBrightness;
+                    var listInsertLocation = orderedBrightnesses.BinarySearch(pixelBrightness);
+                    if (listInsertLocation < 0)
+                    {
+                        listInsertLocation = ~listInsertLocation;
+                    }
+                    orderedPixels.Insert(listInsertLocation, pixelLocation);
+                    orderedBrightnesses.Insert(listInsertLocation, pixelBrightness);
+
+
                     perceivedImage.SetPixel(xLocation, yLocation, Color.FromArgb(255, pixelBrightness, pixelBrightness, pixelBrightness));
-                    knownPixels[new Tuple<int, int>(xLocation, yLocation)] = pixelBrightness;
                 }
 
                 counter++;
-                if(counter > 20000)
+                if(counter > 50000)
                 {
                     counter = 0;
                     perceivedImage.Save(directory + "/Images/laplacedTriangle.png");
